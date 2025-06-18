ae9e79fda1d9898dd65f95f080e9f924ed4c0e85
Jie Lu <jie.lu@tuni.fi>
2025-06-18 17:10:35 +0800

    Add: mapping methods

--- src/main/java/com/it/simplytodo/controller/TodoController.java
+++ src/main/java/com/it/simplytodo/controller/TodoController.java
@@ -1,4 +1,35 @@
 package com.it.simplytodo.controller;
 
+import com.it.simplytodo.Service.TodoService;
+import com.it.simplytodo.entity.TodoTask;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController("/api")
 public class TodoController {
+
+
+    @Autowired
+    private TodoService todoService;
+    // injected the TodoService beans;
+
+    @GetMapping("/task/{id}")
+    public TodoTask getTask(@PathVariable int id){
+        return todoService.getTask(id);
+    }
+    @GetMapping("/tasks")
+    public List<?> getAllTasks(){
+        return  todoService.getAllTasks();
+    }
+    @DeleteMapping("/task/{id}")
+    public void deleteTask(@PathVariable int id){
+        todoService.deleteTask(id);
+    }
+
+    @PostMapping("/task")
+    public TodoTask createTask(@RequestBody TodoTask todoTask){
+        return todoService.createOrUpdate(todoTask);
+    }
 }
