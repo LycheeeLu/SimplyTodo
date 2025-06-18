eea3a9d0e33c88e2563a7e9bd3ed7a4bd2bb091f
Jie Lu <jie.lu@tuni.fi>
2025-06-18 20:26:31 +0800

    Fix: exception handler

--- src/main/java/com/it/simplytodo/errors/TodoErrorController.java
+++ src/main/java/com/it/simplytodo/errors/TodoErrorController.java
@@ -1,18 +1,24 @@
 package com.it.simplytodo.errors;
 
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.PathVariable;
+
 
 @ControllerAdvice
 public class TodoErrorController {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<TodoErrorResponse> handleException (Exception e){
-        TodoErrorResponse
+        TodoErrorResponse errorResponse = new TodoErrorResponse();
+        errorResponse.setMessage(e.getMessage());
+        errorResponse.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+    
+
 }
 
 
