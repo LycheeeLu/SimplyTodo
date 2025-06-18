a2492117acecf4025ac951e7a5ad8f1c7084b9f7
Jie Lu <jie.lu@tuni.fi>
2025-06-18 22:07:13 +0800

    Add: openapi3 notes

---
+++ src/main/java/com/it/simplytodo/config/SwaggerConfig.java
@@ -0,0 +1,3 @@
+
+//annotations will be made in the TodoCOntroller.class
+//Springdoc OpenAPI does not require explicit configuration. It works automatically based on annotations
--- src/main/java/com/it/simplytodo/controller/TodoController.java
+++ src/main/java/com/it/simplytodo/controller/TodoController.java
@@ -2,6 +2,8 @@ package com.it.simplytodo.controller;
 
 import com.it.simplytodo.Service.TodoService;
 import com.it.simplytodo.entity.TodoTask;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@Tag(name = "Todo Task Controller", description = "Operations on Todo Tasks")
 public class TodoController {
 
 
@@ -17,19 +20,23 @@ public class TodoController {
     // injected the TodoService beans;
 
     @GetMapping("/task/{id}")
+    @Operation(summary = "Get a task by ID", description = "Provide an ID to look up specific task")
     public TodoTask getTask(@PathVariable int id){
         return todoService.getTask(id);
     }
     @GetMapping("/tasks")
+    @Operation(summary = "List all tasks", description = "Provide all tasks")
     public List<?> getAllTasks(){
         return  todoService.getAllTasks();
     }
     @DeleteMapping("/task/{id}")
+    @Operation(summary =  "Delete the todo task based on task id", description = "Pass in task id to delete task" )
     public void deleteTask(@PathVariable int id){
         todoService.deleteTask(id);
     }
 
     @PostMapping("/task")
+    @Operation(summary = "update or create a task", description = "create a new task or update an old one(with specified id)")
     public TodoTask createTask(@RequestBody TodoTask todoTask){
         return todoService.createOrUpdate(todoTask);
     }
