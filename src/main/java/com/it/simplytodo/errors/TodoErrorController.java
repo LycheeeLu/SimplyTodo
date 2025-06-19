b29ab4a0ed59ccdc1ac4799660cab715029e9289
Jie Lu <jie.lu@tuni.fi>
2025-06-19 17:15:58 +0800

    Add: robust TodoErrorHandle exception

--- src/main/java/com/it/simplytodo/errors/TodoErrorController.java
+++ src/main/java/com/it/simplytodo/errors/TodoErrorController.java
@@ -10,12 +10,35 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class TodoErrorController {
 
-    @ExceptionHandler(Exception.class)
+    @ExceptionHandler(TodoException.class)
     public ResponseEntity<TodoErrorResponse> handleException (Exception e){
         TodoErrorResponse errorResponse = new TodoErrorResponse();
         errorResponse.setMessage(e.getMessage());
         errorResponse.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
          return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+    @ExceptionHandler(TodoException.class)
+    public ResponseEntity<TodoErrorResponse> hanbleTodoException (TodoException e){
+        HttpStatus httpStatus;
+
+        switch (e.getStatus()) {
+            case NOT_FOUND:
+                httpStatus = HttpStatus.NOT_FOUND;
+                break;
+            case BAD_REQUEST:
+                httpStatus = HttpStatus.BAD_REQUEST;
+                break;
+            default:
+                httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+                break;
+        }
+
+        TodoErrorResponse errorResponse = new TodoErrorResponse();
+        errorResponse.setMessage(e.getMessage());
+        errorResponse.setStatus(httpStatus);
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
